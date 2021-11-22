d5564fc8ee7ed2177a0e2a43289766eb65184b65
riskapauziah13 <69113175+riskapauziah13@users.noreply.github.com>
2021-11-22 16:00:45 +0700

    menambahkan data
    menambahkan data makanan

--- src/data.js
+++ src/data.js
@@ -4,29 +4,561 @@ const food = {
       nama: 'jagung rebus',
       kalori: 90.2,
     },
+    {
+      nama: 'kentang rebus',
+      kalori: 166,
+    },
+    {
+      nama: 'ketan putih',
+      kalori: 217,
+    },
+    {
+      nama: 'ketupat',
+      kalori: 32,
+    },
+    {
+      nama: 'lontong',
+      kalori: 38,
+    },
+    {
+      nama: 'nasi putih',
+      kalori: 175,
+    },
+    {
+      nama: 'nasi putih kentucky',
+      kalori: 349,
+    },
+    {
+      nama: 'roti tawar serat tinggi',
+      kalori: 149,
+    },
+    {
+      nama: 'singkong rebus',
+      kalori: 146,
+    },
+    {
+      nama: 'telur rebus',
+      kalori: 98,
+    },
+    {
+      nama: 'ubi rebus',
+      kalori: 125,
+    },
+    {
+      nama: 'bubur',
+      kalori: 44,
+    },
+    {
+      nama: 'crakers',
+      kalori: 229,
+    },
+    {
+      nama: 'makaroni',
+      kalori: 91,
+    },
+    {
+      nama: 'mie instant',
+      kalori: 168,
+    },
+    {
+      nama: 'nasi tim',
+      kalori: 88,
+    },
+    {
+      nama: 'nasi uduk',
+      kalori: 506,
+    },
+    {
+      nama: 'roti tawar',
+      kalori: 128,
+    },
+    {
+      nama: 'bihun goreng',
+      kalori: 296,
+    },
+    {
+      nama: 'bubur ayam',
+      kalori: 165,
+    },
+    {
+      nama: 'bubur sum-sum',
+      kalori: 178,
+    },
+    {
+      nama: 'kentang goreng',
+      kalori: 211,
+    },
+    {
+      nama: 'mie goreng',
+      kalori: 321,
+    },
+    {
+      nama: 'nasi goreng',
+      kalori: 267,
+    },
+    {
+      nama: 'soun goreng',
+      kalori: 263,
+    },
+    {
+      nama: 'spaghetti',
+      kalori: 642,
+    },
+    {
+      nama: 'tape singkong',
+      kalori: 260,
+    },
   ],
+
   siapSaji: [
     {
       nama: 'asinan',
       kalori: 208,
     },
+    {
+      nama: 'toge goreng',
+      kalori: 243,
+    },
+    {
+      nama: 'gado-gado',
+      kalori: 295,
+    },
+    {
+      nama: 'ketoprak',
+      kalori: 153,
+    },
+    {
+      nama: 'pempek',
+      kalori: 384,
+    },
+    {
+      nama: 'rawon',
+      kalori: 331,
+    },
+    {
+      nama: 'soto ayam',
+      kalori: 101,
+    },
+    {
+      nama: 'soto padang',
+      kalori: 127,
+    },
+    {
+      nama: 'tongseng',
+      kalori: 331,
+    },
+    {
+      nama: 'hamburger',
+      kalori: 257,
+    },
+    {
+      nama: 'kerupuk palembang',
+      kalori: 168,
+    },
+    {
+      nama: 'kerupuk udang',
+      kalori: 72,
+    },
+    {
+      nama: 'mie bakso',
+      kalori: 302,
+    },
+    {
+      nama: 'nasi tim ayam',
+      kalori: 588,
+    },
+    {
+      nama: 'pizza',
+      kalori: 163,
+    },
+    {
+      nama: 'sate kambing',
+      kalori: 729,
+    },
+    {
+      nama: 'sayur krecek',
+      kalori: 249,
+    },
+    {
+      nama: 'siomay',
+      kalori: 361,
+    },
+    {
+      nama: 'soto betawi',
+      kalori: 135,
+    },
+    {
+      nama: 'soto makasar',
+      kalori: 525,
+    },
+    {
+      nama: 'soto sulung',
+      kalori: 86,
+    },
   ],
+
   lauk: [
     {
       nama: 'arsik',
       kalori: 94.05,
     },
+    {
+      nama: 'ayam panggang bumbu kuning',
+      kalori: 129.4,
+    },
+    {
+      nama: 'ayam panggang',
+      kalori: 164.3,
+    },
+    {
+      nama: 'daging panggang',
+      kalori: 150,
+    },
+    {
+      nama: 'ikan mas pepes',
+      kalori: 143.5,
+    },
+    {
+      nama: 'sambal goreng tempe',
+      kalori: 116,
+    },
+    {
+      nama: 'telur asin rebus',
+      kalori: 138,
+    },
+    {
+      nama: 'telur ayam rebus',
+      kalori: 97,
+    },
+    {
+      nama: 'ati ayam goreng',
+      kalori: 98,
+    },
+    {
+      nama: 'ayam pop',
+      kalori: 265,
+    },
+    {
+      nama: 'bakso daging sapi',
+      kalori: 260,
+    },
+    {
+      nama: 'empal daging',
+      kalori: 147,
+    },
+    {
+      nama: 'ikan bandeng goreng',
+      kalori: 180.7,
+    },
+    {
+      nama: 'ikan baronang goreng',
+      kalori: 107.5,
+    },
+    {
+      nama: 'ikan bawal goreng',
+      kalori: 113.3,
+    },
+    {
+      nama: 'ikan ekor goreng',
+      kalori: 107.8,
+    },
+    {
+      nama: 'ikan kembung goreng',
+      kalori: 87.65,
+    },
+    {
+      nama: 'ikan lele goreng',
+      kalori: 57.5,
+    },
+    {
+      nama: 'ikan patin goreng',
+      kalori: 252.7,
+    },
+    {
+      nama: 'ikan selar goreng',
+      kalori: 63.75,
+    },
+    {
+      nama: 'ikan tenggiri goreng',
+      kalori: 85.3,
+    },
+    {
+      nama: 'ikan teri goreng',
+      kalori: 66,
+    },
+    {
+      nama: 'ikan tuna goreng',
+      kalori: 110,
+    },
+    {
+      nama: 'kerang rebus',
+      kalori: 59,
+    },
+    {
+      nama: 'macaroni shootel',
+      kalori: 177,
+    },
+    {
+      nama: 'tahu bacem',
+      kalori: 147,
+    },
+    {
+      nama: 'telur mata sapi',
+      kalori: 40,
+    },
+    {
+      nama: 'tempe bacem',
+      kalori: 157,
+    },
+    {
+      nama: 'tempe goreng',
+      kalori: 118,
+    },
+    {
+      nama: 'tenggiri bumbu kuning',
+      kalori: 94.4,
+    },
+    {
+      nama: 'udang goreng besar',
+      kalori: 68.25,
+    },
+    {
+      nama: 'sambal goreng tempe teri',
+      kalori: 276,
+    },
+    {
+      nama: 'sambal goreng ati sapi',
+      kalori: 200,
+    },
+    {
+      nama: 'sambal goreng udang',
+      kalori: 123,
+    },
+    {
+      nama: 'sop sapi',
+      kalori: 227,
+    },
+    {
+      nama: 'tahu goreng',
+      kalori: 111,
+    },
+    {
+      nama: 'tahu isi',
+      kalori: 124,
+    },
+    {
+      nama: 'tahu sumedang',
+      kalori: 113,
+    },
+    {
+      nama: 'telur dadar',
+      kalori: 188,
+    },
   ],
+
   buah: [
     {
       nama: 'apel',
       kalori: 92,
     },
+    {
+      nama: 'apel merah',
+      kalori: 82,
+    },
+    {
+      nama: 'belimbing',
+      kalori: 80,
+    },
+    {
+      nama: 'duku',
+      kalori: 81,
+    },
+    {
+      nama: 'jambu air',
+      kalori: 35.4,
+    },
+    {
+      nama: 'jambu biji',
+      kalori: 157,
+    },
+    {
+      nama: 'jeruk medan',
+      kalori: 46,
+    },
+    {
+      nama: 'jeruk pontianak',
+      kalori: 67,
+    },
+    {
+      nama: 'jeruk sunkist',
+      kalori: 40,
+    },
+    {
+      nama: 'mangga manalagi',
+      kalori: 72,
+    },
+    {
+      nama: 'nanas',
+      kalori: 104,
+    },
+    {
+      nama: 'pepaya',
+      kalori: 46,
+    },
+    {
+      nama: 'pir',
+      kalori: 80,
+    },
+    {
+      nama: 'pisang rebus',
+      kalori: 136.5,
+    },
+    {
+      nama: 'salak',
+      kalori: 63.6,
+    },
+    {
+      nama: 'semangka',
+      kalori: 48,
+    },
+    {
+      nama: 'alpukat',
+      kalori: 85,
+    },
+    {
+      nama: 'anggur',
+      kalori: 60,
+    },
+    {
+      nama: 'lengkeng',
+      kalori: 79,
+    },
+    {
+      nama: 'melon',
+      kalori: 46,
+    },
+    {
+      nama: 'mangga harum manis',
+      kalori: 90,
+    },
+    {
+      nama: 'pir hijau',
+      kalori: 105,
+    },
+    {
+      nama: 'pisang ambon',
+      kalori: 74.2,
+    },
+    {
+      nama: 'pisang barangan',
+      kalori: 236,
+    },
+    {
+      nama: 'pisang mas',
+      kalori: 11,
+    },
+    {
+      nama: 'pisang raja',
+      kalori: 126,
+    },
+    {
+      nama: 'sirsak',
+      kalori: 55,
+    },
+    {
+      nama: 'durian montong',
+      kalori: 134,
+    },
+    {
+      nama: 'rambutan',
+      kalori: 69,
+    },
+    {
+      nama: 'sawo',
+      kalori: 92,
+    },
   ],
+
   sayur: [
     {
       nama: 'acar kuning',
       kalori: 53,
+    },
+    {
+      nama: 'bening bayam',
+      kalori: 18,
+    },
+    {
+      nama: 'cah labu siam',
+      kalori: 41.6,
+    },
+    {
+      nama: 'sayur asam',
+      kalori: 88,
+    },
+    {
+      nama: 'sop ayam kombinasi',
+      kalori: 95,
+    },
+    {
+      nama: 'sop bayam',
+      kalori: 78,
+    },
+    {
+      nama: 'sop kimmlo',
+      kalori: 104,
+    },
+    {
+      nama: 'sop mutiara jagung',
+      kalori: 113,
+    },
+    {
+      nama: 'asop oyong misoa',
+      kalori: 106,
+    },
+    {
+      nama: 'sop telur putih',
+      kalori: 116,
+    },
+    {
+      nama: 'buntil',
+      kalori: 106,
+    },
+    {
+      nama: 'gudeg',
+      kalori: 132,
+    },
+    {
+      nama: 'sayur lodeh',
+      kalori: 61,
+    },
+    {
+      nama: 'cah jagung putren',
+      kalori: 59,
+    },
+    {
+      nama: 'cah kacang panjang',
+      kalori: 72,
+    },
+    {
+      nama: 'sop oyong telur puyuh',
+      kalori: 134,
+    },
+    {
+      nama: 'setup kentang buncis',
+      kalori: 95,
+    },
+    {
+      nama: 'tumis buncis',
+      kalori: 52,
+    },
+    {
+      nama: 'tumis daun singkong',
+      kalori: 151,
+    },
+    {
+      nama: 'tumis kc.panjang + jagung',
+      kalori: 118,
     },
   ],
 };
