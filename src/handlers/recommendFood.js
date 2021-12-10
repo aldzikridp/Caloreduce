0f13611e9e7da969e48fc4822914ec8fd3635bca
Eorclus <aldzikridp@gmail.com>
2021-12-10 11:01:46 +0700

    jangan masukkan kembali makanan yg sudah disarankan

--- src/handlers/recommendFood.js
+++ src/handlers/recommendFood.js
@@ -2,6 +2,7 @@ const foods = require('../data');
 
 const getNearestVal = (foodsToCompare, calorie) => {
   let currentFood = foodsToCompare[0];
+  let returnIndex;
 
   for (let index = 1; index < foodsToCompare.length; index += 1) {
     const aDiff = Math.abs(currentFood.kalori - calorie);
@@ -10,24 +11,29 @@ const getNearestVal = (foodsToCompare, calorie) => {
     if (aDiff === bDiff) {
       if (foodsToCompare[index].kalori > currentFood.kalori) {
         currentFood = foodsToCompare[index];
+        returnIndex = index;
       }
     } else if (bDiff < aDiff) {
       currentFood = foodsToCompare[index];
+      returnIndex = index;
     }
   }
 
-  return currentFood;
+  return { returnIndex, currentFood };
 };
 
 const getFoodList = (targetCal, foodsToCompare) => {
   let currentCal = 0;
   const recommendedFood = [];
+  const foodList = foodsToCompare;
 
   while (currentCal <= targetCal) {
     const requiredCalorieLeft = targetCal - currentCal;
-    const getFood = getNearestVal(foodsToCompare, requiredCalorieLeft);
-    currentCal += getFood.kalori;
-    recommendedFood.push(getFood);
+    const evaluateFood = getNearestVal(foodList, requiredCalorieLeft);
+    const { returnIndex: index, currentFood: food } = evaluateFood;
+    currentCal += food.kalori;
+    recommendedFood.push(food);
+    foodList.splice(index, 1);
   }
 
   return recommendedFood;
