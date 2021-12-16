9dc1c8ed72aca9a484f5db55faeec0e22c912ccd
Eorclus <aldzikridp@gmail.com>
2021-12-16 10:13:30 +0700

    ubah nix-shell environment

--- shell.nix
+++ shell.nix
@@ -2,9 +2,11 @@ with (import <nixpkgs> {});
 mkShell {
 	buildInputs = [
       nodejs
+      heroku
 	];
 shellHook = ''
     export PATH="./node_modules/.bin:$PATH"
     export DEBUG=http:*
+    export PORT=3000
 '';
 }
