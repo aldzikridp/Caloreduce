c83df749e22c2b59dbd4e0c561e029110dbad96a
Eorclus <aldzikridp@gmail.com>
2021-12-16 10:13:43 +0700

    ubah port dan host

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: '0.0.0.0',
   });
 
   await server.register([Vision, Inert]);
